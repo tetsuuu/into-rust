2b70bc04c11e1d54f7b0b880a6a2ae4373baf1a3
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-12-30 22:43:53 +0900

    til getting start

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,8 +1,29 @@
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+struct Circle {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl HasArea for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
+
+fn print_area<T: HasArea>(shape: T) {
+    println!("This shape has an area of {}", shape.area());
+}
+
 fn main() {
-    hello("hello, world from function");
-}
+    let c = Circle {
+        x: 0.0f64,
+        y: 0.0f64,
+        radius: 1.0f64,
+    };
 
-fn hello(message: &str) {
-    println!("{}",message);
+    print_area(c);
 }
-
