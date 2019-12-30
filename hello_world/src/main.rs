f8cdd5dee9d93df237ed4e5e5a4b4d4b65c41c99
uluru-kinoshitat <t_kinoshita@uluru.jp>
2019-12-30 13:00:20 +0900

    initial commit

---
+++ hello_world/src/main.rs
@@ -0,0 +1,12 @@
+// extern crate chrono;
+
+// use chrono::{Utc, Local, DateTime, Date};
+
+fn main() {
+    hello("hello, world from function");
+}
+
+fn hello(message: &str) {
+    println!("{}",message);
+}
+
